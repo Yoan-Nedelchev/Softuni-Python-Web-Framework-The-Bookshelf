beb7df535af3367351b0cd3ec55e1f64f2037995
Yoan-Nedelchev <yoan.nedelchev@yahoo.com>
2022-12-22 22:02:16 +0200

    -Minor URL change, bug fix for displayed logged-in user

--- theBookshelf/common/urls.py
+++ theBookshelf/common/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from theBookshelf.common.views import index, search, CreateFeedbackView, about_us, statistics
 
 urlpatterns = (
-    path('home', index, name='index'),
+    path('', index, name='index'),
     path('search/', search, name='search'),
     path('feedback/', CreateFeedbackView.as_view(), name='feedback'),
     path('about/', about_us, name='about'),
